29bff7fe1733c31aeb24e2070d5fbdfc7d7fbc76
Kuntash <kuntashtashi11@gmail.com>
2022-05-07 18:39:17 +0530

    Removed env file from git

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-
 import dotenv from 'dotenv';
 
 dotenv.config({
-  path: `${__dirname}/config.env`
+  path: `${__dirname}/config.env`,
 });
 import app from './app';
+
 console.log(process.env.PORT);
 const port = process.env.PORT || 3000;
 
